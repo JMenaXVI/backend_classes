0c56e2f980953158ac18e544912e5438b438ebaa
JMenaXVI <juan_diego.cuevas@uao.edu.co>
2023-04-12 15:20:44 -0500

    Update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('dotenv').config()
 
 // Crear Express App
 const app = express();
 
 //Rutas
+app.use( express.static('public'))
 app.get('/', (req, res) => {
     res.json({
         ok: true
@@ -11,6 +13,6 @@ app.get('/', (req, res) => {
 })
 
 // Escuchar en puerto 4000
-app.listen(4000, () => {
-    console.log('Servidor corriendo en puerto', 4000);
+app.listen( process.env.PORT , () => {
+    console.log('Servidor corriendo en puerto', process.env.PORT)
 })
