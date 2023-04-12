dde6c22b531ad8b2b13886bc91374f3ec4b904b6
JMenaXVI <juan_diego.cuevas@uao.edu.co>
2023-04-12 15:04:16 -0500

    Index actualizado, .gitignore añadido

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Backend started.");
+const express = require('express');
+
+// Crear Express App
+const app = express();
+
+//Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+})
+
+// Escuchar en puerto 4000
+app.listen(4000, () => {
+    console.log('Servidor corriendo en puerto', 4000);
+})
